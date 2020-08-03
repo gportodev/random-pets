6f97d715ee5960fa77e939c50e0f4a6b37852776
gamonpo <gamonpo@outlook.com>
2020-08-03 16:23:12 -0300

    Fim

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  SafeAreaView, 
+  ScrollView, 
+  Image, 
+  ActivityIndicator,
+  RefreshControl, 
+} from 'react-native';
+
+import Constants from 'expo-constants'
+
+import randomColor from 'randomcolor'
 
 export default function App() {
+  const [loading, setLoading] = useState(false)
+  const [pet, setPet] = useState();
+  const changeColor = randomColor()
+
+
+  const loadPet = async () => {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random")
+    const data = await res.json();
+    await Image.prefetch(data.message)
+    setPet(data.message)
+    setLoading(false)
+  }
+
+  useEffect(()=> {
+    loadPet()
+    changeColor
+  }, [])
+
+  if(!pet) return <ActivityIndicator size="large" />
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={loadPet} />
+        }
+      >
+        <Image style={styles.pic} source={{ uri: pet }} />
+        <Text style={[styles.paragraph, { color: changeColor }]}>Dog</Text>
+      </ScrollView>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,6 +55,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
     justifyContent: 'center',
+    padding: 8
   },
+  paragraph: {
+    margin: 25,
+    fontSize: 50,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  pic: {
+    marginTop: 40,
+    height: 500,
+    width: 400,
+    borderRadius: 15,
+  }
 });
